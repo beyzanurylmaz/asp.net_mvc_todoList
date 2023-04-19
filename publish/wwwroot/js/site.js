e0be5d7fade0c33ecc2b1a97000a62ef0f0120d7
Beyzanur <90965586+beyzanurylmaz@users.noreply.github.com>
2023-04-19 17:07:50 +0300

    Add files via upload

---
+++ publish/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿$ (function() {
+    
+    $(".btn-close").click(function () {
+        var btn=$(this);
+        var id = btn.data("id");
+        var action = btn.data("action");
+        var url = "/"+action+"/"+id;
+
+        //alert(url);
+
+        $.ajax({
+            type:"get",
+            url:url,
+
+            success:function (data) {
+                //alert(data);
+
+                btn.parent().remove();
+            }
+        })
+    })
+
+    $("li").click(function () {
+        var btn=$(this);
+        var id = btn.data("id");
+        var action = btn.data("action");
+        var url = "/"+action+"/"+id;
+
+        $.ajax({
+            type:"get",
+            url:url,
+
+            success:function (data) {
+                //alert(data);
+
+                if (data=="False") 
+                {
+                    btn.attr("class", "m-1 list-group-item list-group-item-success")
+                }
+                else
+                {
+                    btn.attr("class", "m-1 list-group-item list-group-item-danger text-decoration-line-through")
+                }
+
+            }
+        })
+    })
+})
